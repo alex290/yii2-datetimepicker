7f094d8d9ef37ee85299dd7de0c1044e22598ee8
alex <alex@webgoal.ru>
2021-02-18 17:00:20 +0300

    Fix bug old time

--- scr/js/main.js
+++ scr/js/main.js
@@ -3,9 +3,23 @@ $(document).ready(function() {
     $('.date_time_pick_wrap').each(function(index, element) {
         var objWrap = $(this);
 
-        var dataValue = objWrap.find('.outDateTime').val();
-        let arr = new Date(dataValue);
-        objWrap.find('.inputTime').val(arr.getTime() / 1000);
+        var dataValueOld = objWrap.find('.inputTime').val();
+        if (dataValueOld > 0) {
+            let arr = new Date(dataValueOld * 1000);
+
+            let dateTimeD = arr.toISOString();
+            let arDatD = dateTimeD.split('T');
+            let arTimeD = arDatD[1].split('.')[0].split(':');
+            let valData = arDatD[0] + 'T' + arTimeD[0] + ':' + arTimeD[1];
+            // console.log(valData);
+            objWrap.find('.outDateTime').val(valData);
+        } else {
+            var dataValue = objWrap.find('.outDateTime').val();
+            let arr = new Date(dataValue);
+            let timestamp = arr.getTime() / 1000;
+            objWrap.find('.inputTime').val(timestamp);
+        }
+
 
         objWrap.find('.outDateTime').on('change', function() {
             let datetime = $(this).val();
